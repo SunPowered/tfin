c4d9c456969cae27b08b30a88a61402e0700797f
Tim van Boxtel <tim@vanboxtel.ca>
2021-09-25 13:00:57 -0400

    Chart of Accounts added.
    This is an account management object that holds relevant accounts for
    the simulation.  It organizes the accounts in a dict with key structure:
    
    `accounts[ACCOUNT_TYPE][ACCOUNT_NAME]`
    
    where both `ACCOUNT_TYPE` and `ACCOUNT_NAME` are strings.
    
    There are convenience methods for adding, removing, and checking
    existence of an account.  Most methods allow for passing account and
     account_type by a string of the name or a direct object.
    
     There are also some filtering methods to help retrieve relevant data
     retrievals.

--- tests/test_accounting.py
+++ tests/test_accounting.py
@@ -27,12 +27,23 @@ def expense():
     return Expense("Test Expense Account")
 
 
+@pytest.fixture
+def coa():
+    return ChartOfAccounts()
+
+
 def test_account_types():
     """Checks the Account typing design"""
     cash_acct = Asset(name="Cash")
 
     assert isinstance(cash_acct, Account), "cash_acct not instance Type[Account]"
     assert issubclass(Asset, Account), "Asset class not a subclassof Account"
+
+
+def test_accounts_mapping():
+    """Checks the account mapping exists and returns appropriate results"""
+    assert accounts_by_type, "Empty accounts mapping"
+    assert accounts_by_type[AccountType.INCOME] == Income, "Check the accounts mapping"
 
 
 def test_debit_credit(asset, expense, income):
@@ -54,3 +65,69 @@ def test_debit_credit(asset, expense, income):
 
     assert asset.balance == 105
     assert income.balance == 25
+
+
+def test_coa_account_management(coa, asset):
+    """Test the account management handling of the COA"""
+    assert len(coa) == 0, "Non empty accounts initialized in COA"
+    assert not coa.has_account(asset), "has_account method incorrectly returning"
+
+    coa.add_account(asset)
+    assert len(coa) == 1, "Account not added or available from __len__"
+    assert coa.has_account(asset), "has_account method incorrectly returning"
+
+    coa.remove_account(asset)
+    assert len(coa) == 0, "Account not removed"
+
+    coa.add_account("Test Account")
+    assert len(coa) == 0, "Bad account was added"
+
+    coa.add_account(asset)
+    coa.remove_account("Test Asset")
+    assert coa.has_account(asset), "Bad account name still removed an account"
+
+
+def test_coa_create_account(coa):
+    """Test the create_account method on the ChartOfAccounts"""
+    account = coa.create_account(
+        account_type=AccountType.EXPENSE,
+        account_name="Expense Account",
+        starting_balance=55,
+    )
+
+    assert account, "No account returned from create_account"
+    assert len(coa) == 1, "Account created not added to COA"
+    assert (
+        account == coa.accounts["EXPENSE"]["Expense Account"]
+    ), "account was not indexed to the correct location"
+    coa.remove_account(account)
+    assert len(coa) == 0, "Created account not removed"
+
+    account = coa.create_account(
+        account_type="income", account_name="Good Account Type"
+    )
+    assert account, "Account not created by account_type string"
+    coa.remove_account(account)
+
+    account = coa.create_account(
+        account_type="not_a_type", account_name="Bad Account Type"
+    )
+    assert not account, "Account shouldnt be created with a bad AccountType"
+    assert len(coa) == 0, "Should not have added bad account"
+
+
+def test_coa_by_name_and_type(coa, asset):
+    """Tests the COA by_name_and_type method"""
+    coa.add_account(asset)
+
+    account = coa.by_name_and_type(asset.name, asset.account_type)
+    assert account == asset, "COA by_name_and_type did not return the Account object"
+
+    account = coa.by_name_and_type("Not in Here", "expense")
+    assert not account, "Account returned when none exists with that name"
+
+    account = coa.by_name_and_type("Test Account", "expense")
+    assert not account, "Account returned when none exists in that account type"
+
+    account = coa.by_name_and_type({}, 2)
+    assert not account, "Account returned when garbage data given"
--- tfin/accounting.py
+++ tfin/accounting.py
@@ -2,9 +2,19 @@
 
 from dataclasses import dataclass
 from enum import Enum, auto
-from typing import Callable, Protocol, Union
+from typing import Callable, Dict, Optional, Protocol, Type, Union, cast
 
-__all__ = ["Account", "Asset", "Liability", "Equity", "Income", "Expense"]
+__all__ = [
+    "Account",
+    "Asset",
+    "Liability",
+    "Equity",
+    "Income",
+    "Expense",
+    "AccountType",
+    "accounts_by_type",
+    "ChartOfAccounts",
+]
 
 
 class AccountType(Enum):
@@ -29,7 +39,7 @@ N = Union[float, int]
 C = Callable[[float, float], float]
 
 
-class AccountInterface(Protocol):
+class AccountInterface(Protocol):  # pragma: no cover
     """The Account Protocol interface"""
 
     account_type: AccountType
@@ -122,3 +132,99 @@ class Expense(Account, AssetLike):
     """Expense Account"""
 
     account_type = AccountType.EXPENSE
+
+
+accounts_by_type: Dict[AccountType, Type[Account]] = {
+    AccountType.ASSET: Asset,
+    AccountType.LIABILITY: Liability,
+    AccountType.EQUITY: Equity,
+    AccountType.INCOME: Income,
+    AccountType.EXPENSE: Expense,
+}
+
+
+class ChartOfAccounts:
+    """A chart of accounts that can manage and filter accounts"""
+
+    def __init__(self):
+
+        self._accounts: Dict[str, Dict[str, Account]] = dict(
+            ((act_type.name, {}) for act_type in AccountType)
+        )
+
+    def __len__(self):
+        """Return the total number of accounts under management"""
+        return sum([len(self._accounts[acc_type]) for acc_type in self._accounts])
+
+    def _convert_account_type(
+        self, account_type: Union[AccountType, str]
+    ) -> Optional[AccountType]:
+        """Convert a string account type to a proper AccountType Object"""
+
+        if isinstance(account_type, AccountType):
+            return account_type
+
+        if isinstance(account_type, str):
+            try:
+                return AccountType[account_type.upper()]
+            except KeyError:
+                ...
+        return None
+
+    @property
+    def accounts(self):
+        return self._accounts
+
+    def add_account(self, account: Account):
+        """Add an instantiated account to the chart"""
+        if isinstance(account, Account):
+            self._accounts[account.account_type.name][account.name] = account
+
+    def remove_account(self, account: Account):
+        """Remove an account from the chart"""
+        if not isinstance(account, Account):
+            return
+        accounts = self.by_type(account.account_type)
+        if accounts and account.name in accounts:
+            del self._accounts[account.account_type.name][account.name]
+
+    def create_account(
+        self,
+        account_type: Union[AccountType, str],
+        account_name: str,
+        starting_balance: N = 0.0,
+    ) -> Optional[Account]:
+        """Create and add a new account from its constructor parameters and add return it"""
+
+        cast_account_type = self._convert_account_type(account_type)
+        if not cast_account_type:
+            return None
+
+        account = accounts_by_type[cast_account_type](
+            name=account_name, starting_balance=float(starting_balance)
+        )
+        self.add_account(account)
+        return account
+
+    def has_account(self, account: Account) -> bool:
+        """Method to see whether an instantiated account is present in the coa"""
+        return account.name in self._accounts[account.account_type.name]
+
+    def by_type(
+        self, account_type: Union[AccountType, str]
+    ) -> Optional[Dict[str, Account]]:
+        """Returns a dict of accounts with a given account type.
+
+        You can pass an AccountType directly, or a string of the account type"""
+
+        cast_account_type = self._convert_account_type(account_type)
+        return self._accounts[cast_account_type.name] if cast_account_type else None
+
+    def by_name_and_type(
+        self,
+        account_name: str,
+        account_type: Union[AccountType, str],
+    ) -> Optional[Account]:
+
+        accounts = self.by_type(account_type)
+        return accounts.get(account_name, None) if accounts else None
